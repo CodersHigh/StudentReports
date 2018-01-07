08ce28524b9ff8b4af8b05960e6326a5026a7c7f
Ryu Hyo Kwang <hkryu0719@gmail.com>
2018-01-08 01:47:24 +0900

    보고서 디자인 1

--- commons/report.js
+++ commons/report.js
@@ -27,7 +27,7 @@ function getContents() {
 function fillHeader() {
 
     var content = {};
-    var index_content = 0; 
+    var index_content = 0;
     for (index_content = 0; index_content < contents.length; index_content++) {
         var content_temp = contents[index_content];
         if (content_temp.lang === "ko") {
@@ -42,32 +42,43 @@ function fillHeader() {
     var course = content.course;
     var period = content.period;
     var instructor = content.instructor;
+    var appName = content.appName;
+    var partner = content.partner
 
     var code = "<div class='top'>\
-    <div>\
-        <span>No.</span>\
-        <span>" + report_no + "</span>\
+    <div class='ribbon'>\
     </div>\
+    <div class='num'>\
+        No. " + report_no + "\
+    </div>\
+    </div>\
+    <div class='title'>\
+        <p>STUDENT REPORT</p>\
+        <p><span class='title_korean'>학생성과 보고서</span></p>\
     </div>\
     <div class='name'>" + name + "</div>\
     <div class='description'>\
         <p>위 학생은 코더스하이 교육과정을</p>\
         <p>성공적으로 수료하였습니다.</p>\
     </div>\
-    <div>\
+    <div class='course'>\
         <div>\
-            <div>교육과정</div>\
-            <div>" + course + "</div>\
+            <div>교육과정 : <span class='word-spacing'>" + course + "</span></div>\
         </div>\
         <div>\
-            <div>교육기간</div>\
-            <div>" + period +  "</div>\
+            <div>교육기간 : <span class='word-spacing'>" + period + "</span></div>\
         </div>\
         <div>\
-            <div>강사</div>\
-            <div>" + instructor + "</div>\
+            <div><span class='word-spacing'>강사 : </span><span class='word-spacing'>" + instructor + "</span></div>\
         </div>\
-    </div>";
+    </div>\
+    <div class='spacing'></div>\
+    <div class='report-detail'>상세 보고서</div>\
+    <div class='detail-content'>\
+        <div>App 이름 : " + appName + "</div>\
+        <div>팀원 : " + partner + " </div>\
+    </div>\
+    ";
     document.getElementById("certificateForm").innerHTML = code;
 }
 
