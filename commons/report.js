f49e8f5f89e7c65becc91ff2ce0ec162bcc7d088
Ryu Hyo Kwang <hkryu0719@gmail.com>
2018-01-13 14:47:49 +0900

    한영전환(버튼식)

--- commons/report.js
+++ commons/report.js
@@ -1,6 +1,6 @@
 var contents = {};
 
-function getContents() {
+function setKor() {
     var content_url = "content.json";
 
     $.support.cors = true;
@@ -10,8 +10,7 @@ function getContents() {
         console.log("second success");
         if (json && json.contents) {
             contents = json.contents;
-            fillHeader();
-            fillFooter();
+            fillHeaderKor();
         }
         else {
             // parse error
@@ -24,7 +23,33 @@ function getContents() {
     });
 }
 
-function fillHeader() {
+function setEng() {
+    var content_url = "content.json";
+
+    $.support.cors = true;
+    $.getJSON(content_url, function () {
+        console.log("success");
+    }).done(function (json) {
+        console.log("second success");
+        if (json && json.contents) {
+            contents = json.contents;
+            fillHeaderEng();
+            fillArticle();
+        }
+        else {
+            // parse error
+            console.log("parse error");
+        }
+    }).fail(function (jqxhr, textStatus, error) {
+        console.log("error");
+    }).always(function () {
+        console.log("complete");
+    });
+
+}
+
+
+function fillHeaderKor() {
 
     var content = {};
     var index_content = 0;
@@ -56,13 +81,13 @@ function fillHeader() {
     var commit3 = content.commit3;
 
 
-
-
     var code = "<div class='top'>\
-    <div class='ribbon'>\
-    </div>\
     <div class='num'>\
         No. " + report_no + "\
+    </div>\
+    <div class='lang_button'>\
+    <input type='button' value='KOR' onClick='setKor()'>\
+    <input type='button' value='ENG' onClick='setEng()'>\
     </div>\
     </div>\
     <div class='report_title'>\
@@ -180,11 +205,169 @@ function fillHeader() {
     document.getElementById("certificateForm").innerHTML = code;
 }
 
-function fillFooter() {
+
+function fillHeaderEng() {
+
+    var content = {};
+    var index_content = 0;
+    for (index_content = 0; index_content < contents.length; index_content++) {
+        var content_temp = contents[index_content];
+        if (content_temp.lang === "en") {
+            content = content_temp.content;
+        }
+    }
+
+    console.log(content);
+
+    var report_no = content.report_no;
+    var name = content.name;
+    var course = content.course;
+    var period = content.period;
+    var instructor = content.instructor;
+    var appName = content.appName;
+    var partner = content.partner;
+    var appIntro = content.appIntro;
+    var testPeriod = content.testPeriod;
+    var testScore = content.testScore;
+    var appStore = content.appStore;
+    var website = content.website;
+    var sourceCode = content.sourceCode;
+    var video = content.video;
+    var commit1 = content.commit1;
+    var commit2 = content.commit2;
+    var commit3 = content.commit3;
+
+
+    var code = "<div class='top'>\
+    <div class='num'>\
+        No. " + report_no + "\
+    </div>\
+    <div class='lang_button'>\
+    <input type='button' value='KOR' onClick='setKor()'>\
+    <input type='button' value='ENG' onClick='setEng()'>\
+    </div>\
+    </div>\
+    <div class='report_title'>\
+        <div class='title_eng'><p>STUDENT REPORT</p></div>\
+    </div>\
+    <div class='name'>" + name + "</div>\
+    <div class='description'>\
+        <p>The student above has successfully completed</p>\
+        <p>the course of CodersHigh.</p>\
+    </div>\
+    <div class='course'>\
+        <div>\
+            <div class='course-spacing'>Course : <span class='word-spacing'>" + course + "</span></div>\
+        </div>\
+        <div>\
+            <div class='period-spacing'>Period : <span class='word-spacing'>" + period + "</span></div>\
+        </div>\
+        <div>\
+            <div>Instructor : </span><span class='word-spacing'>" + instructor + "</span></div>\
+        </div>\
+    </div>\
+    <div class='spacing'></div>\
+    <div class='report-detail'>Detailed Report</div>\
+    <div class='detail-content'>\
+        <div>App Name : " + appName + "</div>\
+        <div>Partner : " + partner + " </div>\
+    </div>\
+    <div class='container spacing-md'>\
+        <div class='title'>\
+            <h3>App Information</h3>\
+        </div>\
+        <div class='row'>\
+            <div class='col-xs-5 centered'>\
+                <img src='detail/appImage.png'>\
+            </div>\
+            <div class='col-xs-7 centered lefted'>\
+                <div class='spacing-xs'>\
+                    <img src='detail/appIcon.png'>\
+                    <h2>" + appName + "</h2>\
+                    <p>\
+                    " + appIntro + "\
+                    </p>\
+                    <a href='" + appStore + "'>\
+                        <img src='../../../commons/goToAppStore.png' class='gotoapp' />\
+                    </a>\
+                </div>\
+                <h4>Project Website : <a href='" + website + "'>" + website + "</a></h4>\
+                <h4>Source Code : <a href='" + sourceCode + "'>" + sourceCode + "</a></h4>\
+            </div>\
+        </div>\
+    </div>\
+    <div class='container spacing-md'>\
+        <div class='title spacing-sm'>\
+            <h3>Presentation Video</h3>\
+        </div>\
+        <div class='centered'>\
+            <iframe width='560' height='315' src='" + video + "' frameborder='0' gesture='media' allow='encrypted-media' allowfullscreen></iframe>\
+        </div>\
+    </div>\
+    <div class='container spacing-md'>\
+        <div class='title spacing-sm'>\
+            <h3>Contribution</h3>\
+        </div>\
+        <div class='centered'>\
+            <img src='detail/contribute.png' style='width:40%'' />\
+        </div>\
+    </div>\
+    <div class='container spacing-md'>\
+        <div class='title'>\
+            <h3>Team Project Working</h3>\
+        </div>\
+        <div class='subtitle'>\
+            <h4>Issue Created</h4>\
+        </div>\
+        <div class='centered spacing-xs'>\
+            <img src='detail/issueCreated.png' style='width:80%''/>\
+        </div>\
+        <div class='subtitle'>\
+            <h4>Issue Closed</h4>\
+        </div>\
+        <div class='centered'>\
+            <img src='detail/issueClosed.png' style='width:80%'/>\
+        </div>\
+    </div>\
+    <div class='container spacing-md'>\
+        <div class='title'>\
+            <h3>Noteworthy Commits</h3>\
+        </div>\
+        <div class='noteworthy-commit'>\
+            1. alert modify : <a href='" + commit1 + "'>" + commit1 + "</a>\
+        </div>\
+        <div class='noteworthy-commit'>\
+            2. table cell : <a href='" + commit2 + "'>" + commit2 + "</a>\
+        </div>\
+        <div class='noteworthy-commit'>\
+            3. registration : <a href='" + commit3 + "'>" + commit3 + "</a>\
+        </div>\
+    </div>\
+    <div class='container spacing-md'>\
+        <div class='title'>\
+            <h3>Development Learning</h3>\
+        </div>\
+        <div class='subtitle'>\
+            <h4>Swift Quiz</h4>\
+        </div>\
+        <div class='test'>\
+            Date : " + testPeriod + "\
+        </div>\
+        <div class='test'>\
+            Score : " + testScore + "\
+        </div>\
+    </div>\
+    ";
+    document.getElementById("certificateForm").innerHTML = code;
+}
+
+function fillArticle() {
 
 }
 
 window.onload = function () {
-    getContents();
+    setKor();
+
+
     // $("#report_detail").load("detail/report_detail.html");
 };
